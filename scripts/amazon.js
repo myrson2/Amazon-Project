6d4d8a41bd0d0aeef8c4ec0ab6cd287e39dafdbf
Jose Myrson O. Beros <beros.josemyrson.oreo1@gmail.com>
2026-05-28 15:39:17 +0800

    import products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import {cart} from '../data/cart.js'
+import { cart } from '../data/cart.js'
+import { products } from '../data/products.js';
 
 const product_grid = document.querySelector('.js-products-grid')
 let displayProduct = ``
